a85dbcc5061a813038b5b689a729d459f9663b2a
Arthur Almeida <arthurgva1602@gmail.com>
2026-08-01 13:03:26 -0300

    feat(database): export GitHub observation schema

--- packages/database/src/schema/index.ts
+++ packages/database/src/schema/index.ts
@@ -1,6 +1,7 @@
 export * from "./audit";
 export * from "./auth";
 export * from "./content";
+export * from "./github";
 export * from "./operations";
 export * from "./projects";
 export * from "./roadmap";
