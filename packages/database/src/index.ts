5c4607f95d6ad0d3ead6179905901cb7b86d875a
Arthur Almeida <arthurgva1602@gmail.com>
2026-08-01 10:55:33 -0300

    feat(database): export overview data source

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,6 +1,7 @@
 export { createSqliteDatabase, migrate } from "./adapters/sqlite";
 export type { SqliteDatabase } from "./adapters/sqlite";
 export { SqliteAuthSessionStore } from "./repositories/auth-session-store";
+export { SqliteOverviewDataSource } from "./repositories/overview-data-source";
 export { SqliteProjectRepository } from "./repositories/project-repository";
 export { SqliteStageRepository } from "./repositories/stage-repository";
 export * as schema from "./schema";
