0b7b1f9daaa2780a46099773a30a45822baa1c13
Arthur Almeida <arthurgva1602@gmail.com>
2026-08-01 13:00:57 -0300

    feat(github): export read-only REST client

---
+++ packages/github/src/index.ts
@@ -0,0 +1,16 @@
+export {
+  GITHUB_API_VERSION,
+  GitHubClientError,
+  GitHubRestClient,
+} from "./github-rest-client";
+export type {
+  GitHubBranch,
+  GitHubBranchPage,
+  GitHubClientErrorCode,
+  GitHubCommitObservation,
+  GitHubRateLimit,
+  GitHubRepository,
+  GitHubResponseMeta,
+  GitHubRestClientOptions,
+  GitHubResult,
+} from "./github-rest-client";
