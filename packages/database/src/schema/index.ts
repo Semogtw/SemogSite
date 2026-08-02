0969ef45e5ed3f6878c004b5fe1bbb60a3335521
Arthur Almeida <arthurgva1602@gmail.com>
2026-08-02 01:41:48 -0300

    feat(database): export editorial schema

--- packages/database/src/schema/index.ts
+++ packages/database/src/schema/index.ts
@@ -1,6 +1,7 @@
 export * from "./audit";
 export * from "./auth";
 export * from "./content";
+export * from "./editorial";
 export * from "./github";
 export * from "./operations";
 export * from "./projects";
