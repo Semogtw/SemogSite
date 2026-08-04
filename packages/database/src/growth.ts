227645882f2a4929b41da45c3f3b82abad01768d
Arthur Almeida <arthurgva1602@gmail.com>
2026-08-03 21:59:17 -0300

    feat: expose learning growth persistence

---
+++ packages/database/src/growth.ts
@@ -0,0 +1,13 @@
+export { SqliteLearningCheckpointRepository } from "./repositories/learning-checkpoint-repository";
+export { SqliteLearningGoalRepository } from "./repositories/learning-goal-repository";
+export { SqliteSkillRepository } from "./repositories/skill-repository";
+export {
+  learningCheckpointEvents,
+  learningCheckpoints,
+  learningCheckpointSkills,
+  learningGoalEvents,
+  learningGoals,
+  learningGoalSkills,
+  skillAliasEvents,
+  skills,
+} from "./schema/growth";
