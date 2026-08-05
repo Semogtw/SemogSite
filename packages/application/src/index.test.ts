0833ffc3c8fc200082e5f5fdcf69549559e14325
Arthur Almeida <arthurgva1602@gmail.com>
2026-08-05 03:04:07 -0300

    test(application): pin public grant revocation planner

--- packages/application/src/index.test.ts
+++ packages/application/src/index.test.ts
@@ -24,6 +24,7 @@ describe("@semogtw/application public contracts", () => {
   it("exports owner authorization request planners", () => {
     expect(application).toMatchObject({
       validateAgentGrantRequest: expect.any(Function),
+      planAgentGrantRevocation: expect.any(Function),
       planAgentClientRevocation: expect.any(Function),
       planAgentTrustSessionCreation: expect.any(Function),
       planAgentTrustSessionRevocation: expect.any(Function),
