289510924aab99355244453a5ef14b180d87810c
Arthur Almeida <arthurgva1602@gmail.com>
2026-08-19 07:41:40 -0300

    feat(portfolio): discover published project detail pages

--- apps/web/src/routes/sitemap[.]xml.ts
+++ apps/web/src/routes/sitemap[.]xml.ts
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { readPublicProjects } from "../server/public-projects.server";
 import {
   buildPortfolioSitemap,
   normalizePublicOrigin,
@@ -7,13 +8,18 @@ import {
 export const Route = createFileRoute("/sitemap.xml")({
   server: {
     handlers: {
-      GET: async ({ request }) =>
-        new Response(buildPortfolioSitemap(normalizePublicOrigin(request.url)), {
-          headers: {
-            "Cache-Control": "public, max-age=300",
-            "Content-Type": "application/xml; charset=utf-8",
+      GET: async ({ request }) => {
+        const projects = await readPublicProjects();
+        return new Response(
+          buildPortfolioSitemap(normalizePublicOrigin(request.url), projects),
+          {
+            headers: {
+              "Cache-Control": "public, max-age=300",
+              "Content-Type": "application/xml; charset=utf-8",
+            },
           },
-        }),
+        );
+      },
     },
   },
 });
